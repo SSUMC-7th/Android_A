04dc3f92a8ef574168fe1037e0a6ebdbb001e8af
Je-0204 <dlej0204@naver.com>
2024-10-03 01:09:47 +0900

    [ike/#1] mission (MainBanner 짤리는 부분, MiniPlayer 여백 제외)

---
+++ app/src/main/java/umc/study/umc_7th/PromotionBanner.kt
@@ -0,0 +1,55 @@
+package umc.study.umc_7th
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PromotionImageBanner(
+    image: ImageBitmap,
+    padding: Dp = 0.dp,
+    onClicked: () -> Unit,
+) {
+    Box(modifier = Modifier.padding(padding)) {
+        Image(
+            bitmap = image,
+            contentDescription = null,
+            modifier = Modifier.clickable { onClicked() }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPromotionImageBanner_rnb() {
+    PromotionImageBanner(
+        image = ImageBitmap.imageResource(id = R.drawable.img_home_viewpager_exp),
+        onClicked = {},
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPromotionImageBanner_floSelect() {
+    PromotionImageBanner(
+        image = ImageBitmap.imageResource(id = R.drawable.discovery_banner_aos),
+        onClicked = {},
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPromotionImageBanner_yanolja() {
+    PromotionImageBanner(
+        image = ImageBitmap.imageResource(id = R.drawable.img_home_viewpager_exp2),
+        onClicked = {},
+    )
+}
