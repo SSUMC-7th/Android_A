72795c93734113c6a080bd9c2dbab9bc5fb73586
조윤식 <cho.yunsig56@gmail.com>
2024-10-12 00:34:03 +0900

    [totoro/#16] 이미지 캐싱 모듈 개선

--- app/src/main/java/umc/study/umc_7th/ImageLoader.kt
+++ app/src/main/java/umc/study/umc_7th/ImageLoader.kt
@@ -1,24 +1,25 @@
 package umc.study.umc_7th
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import umc.study.umc_7th.network.Server
 
 object ImageLoader {
-    private var idSequence = 0
-        get() = --field
-    private val imageMap = emptyMap<Int, ImageBitmap>().toMutableMap()
+    private val imageMap = emptyMap<Long, ImageBitmap>().toMutableMap()
 
-    @Composable
-    fun getImageBitmap(id: Int): ImageBitmap {
-        if (!imageMap.containsKey(id) && id > 0)
-            imageMap[id] = ImageBitmap.imageResource(id = id)
-        return imageMap[id]!!
-    }
-
-    fun putImageBitmap(image: ImageBitmap): Int {
-        val id = idSequence
-        imageMap[id] = image
-        return id
+    fun getImageBitmap(
+        scope: CoroutineScope,
+        id: Long,
+        onLoaded: (ImageBitmap) -> Unit,
+        refresh: Boolean = false,
+    ) {
+        val image = imageMap[id]
+        if (refresh || image == null) scope.launch {
+            val loadedImage = Server.getImage(id)
+            imageMap[id] = loadedImage
+            onLoaded(loadedImage)
+        }
+        else onLoaded(image)
     }
 }
