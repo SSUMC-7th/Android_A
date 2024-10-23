62cf20145c8a8cbc9f5aceb51999f35d84cc2393
조윤식 <cho.yunsig56@gmail.com>
2024-10-23 20:47:49 +0900

    [totoro/#25] 라이트 테마로 임시 고정

--- app/src/main/java/umc/study/umc_7th/ui/theme/Theme.kt
+++ app/src/main/java/umc/study/umc_7th/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package umc.study.umc_7th.ui.theme
 
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun Umc_7thTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
