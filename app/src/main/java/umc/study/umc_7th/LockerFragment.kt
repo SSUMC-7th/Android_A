bc56c7c18a90de4ae48b9755aa396db3234256e2
Je-0204 <dlej0204@naver.com>
2024-11-02 16:30:07 +0900

    [ike/#30] mission {feat. [오늘 발매 음악] Play 버튼 클릭 시 MiniPlayer에 동기화}

--- app/src/main/java/umc/study/umc_7th/LockerFragment.kt
+++ app/src/main/java/umc/study/umc_7th/LockerFragment.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -30,7 +29,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -142,7 +140,7 @@ fun LockerFragment() {
         },
         bottomBar = {
             Column {
-                MiniPlayer(viewModel = MusicViewModel(), progress = 0f, songTitle, songAuthor)
+                MiniPlayer(viewModel = MusicViewModel(), progress = 0f)
             }
         }
     ) { innerPadding ->
