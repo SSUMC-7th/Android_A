e2f6767edd01da4183539805c08af07d26bd3b27
Je-0204 <dlej0204@naver.com>
2024-11-20 21:09:17 +0900

    [ike/#51] mission {chore. Retrofit2, OkHTTP dependency 추가}

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,14 @@ dependencies {
     implementation(libs.accompanist.pager.indicators)
     implementation(libs.circleindicator)
 
+    // Retrofit 2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHTTP
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
     // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
