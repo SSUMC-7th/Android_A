72116f110e515fc457704a17c3b39a67adb382fa
Je-0204 <dlej0204@naver.com>
2024-11-23 22:06:12 +0900

    [ike/#51] mission {refactor. ApiService 주소 변경}

--- app/src/main/java/umc/study/umc_7th/data/source/remote/ApiService.kt
+++ app/src/main/java/umc/study/umc_7th/data/source/remote/ApiService.kt
@@ -2,7 +2,6 @@ package umc.study.umc_7th.data.source.remote
 
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.POST
 import umc.study.umc_7th.data.model.UserJoinRequest
 import umc.study.umc_7th.data.model.UserJoinResponse
