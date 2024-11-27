bb1d90c309333a87f8c09eb66df38df03d88a17d
deephoon <deephoony@gmail.com>
2024-11-27 22:24:50 +0900

    RoomDB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -142,4 +142,18 @@ dependencies {
     implementation("androidx.room:room-migration:2.6.0")
     implementation("androidx.room:room-runtime:2.6.0")
     kapt("androidx.room:room-compiler:2.6.0")
+
+// Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+// okHttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+// Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 }
--- app/src/main/java/umc/study/umc_7th/Album.kt
+++ app/src/main/java/umc/study/umc_7th/Album.kt
@@ -1,13 +1,13 @@
-package umc.study.umc_7th
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "AlbumTable")
-data class Album(
-    // album의 pk는 임의로 지정해 줄 것이므로, autoGenerate 하지 않음
-    @PrimaryKey(autoGenerate = false) var id: Int = 0,
-    var title: String? = "",
-    var singer: String? = "",
-    var coverImage: Int? = null
-)
+//package umc.study.umc_7th
+//
+//import androidx.room.Entity
+//import androidx.room.PrimaryKey
+//
+//@Entity(tableName = "AlbumTable")
+//
+//data class Album(
+//    @PrimaryKey(autoGenerate = false) var id: Int = 0,
+//    var title: String? = "",
+//    var singer: String? = "",
+//    var coverImage: Int? = null
+//)
